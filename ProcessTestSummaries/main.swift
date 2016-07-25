b97d8f8b584d0ffcdaf6e5b17c7617789f901ffb
Teodor Nacu <teodor.nacu@softvision.ro>
2016-07-25 14:34:23 +0300

    Fixed an issue with generating the Junit report xml, that doesn't load the tests results in Jenkins if invalid XML chars appear in failure message. Also did some small code optimization.

--- ProcessTestSummaries/main.swift
+++ ProcessTestSummaries/main.swift
@@ -8,6 +8,26 @@
 
 import Foundation
 
+private func getInvalidXMLCharacterSet() -> NSCharacterSet {
+    // First, create a character set containing all valid UTF8 characters.
+    let xmlCharacterSet = NSMutableCharacterSet()
+    xmlCharacterSet.addCharactersInRange(NSMakeRange(0x9, 1))
+    xmlCharacterSet.addCharactersInRange(NSMakeRange(0xA, 1))
+    xmlCharacterSet.addCharactersInRange(NSMakeRange(0xD, 1))
+    xmlCharacterSet.addCharactersInRange(NSMakeRange(0x20, 0xD7FF - 0x20))
+    xmlCharacterSet.addCharactersInRange(NSMakeRange(0xE000, 0xFFFD - 0xE000))
+    xmlCharacterSet.addCharactersInRange(NSMakeRange(0x10000, 0x10FFFF - 0x10000))
+
+    // Then create and retain an inverted set, which will thus contain all invalid XML characters.
+    //    invalidXMLCharacterSet = xmlCharacterSet.invertedSet
+    return xmlCharacterSet.invertedSet
+}
+
+// First create a character set containing all invalid XML characters.
+// Create this once and leave it in memory so that we can reuse it rather
+// than recreate it every time we need it.
+private let invalidXMLCharacterSet = getInvalidXMLCharacterSet()
+
 /// Override XCTFail method to avoid importing XCTest framework for JSON extension
 func XCTFail(message: String) {
     try! CustomErrorType.InvalidState(error: message).throwsError()
@@ -58,10 +78,71 @@ func createFolderOrEmptyIfExistsAtPath(path: String, emptyPath: Bool = true) -> 
     return containerSetupSuccess
 }
 
+func validXMLString(string: String) -> String {
+    // Not all UTF8 characters are valid XML.
+    // See:
+    // http://www.w3.org/TR/2000/REC-xml-20001006#NT-Char
+    // (Also see: http://cse-mjmcl.cse.bris.ac.uk/blog/2007/02/14/1171465494443.html )
+    //
+    // The ranges of unicode characters allowed, as specified above, are:
+    // Char ::= #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF] /* any Unicode character, excluding the surrogate blocks, FFFE, and FFFF. */
+    //
+    // To ensure the string is valid for XML encoding, we therefore need to remove any characters that
+    // do not fall within the above ranges.
+
+    // Are there any invalid characters in this string?
+    var range = string.rangeOfCharacterFromSet(invalidXMLCharacterSet)
+
+    // If not, just return self unaltered.
+    if range == nil || range?.count == 0 {
+        return string
+    }
+
+    // Otherwise go through and remove any illegal XML characters from a copy of the string.
+    var cleanedString = string
+
+    while range != nil && range?.count > 0 {
+        cleanedString.removeRange(range!)
+        range = cleanedString.rangeOfCharacterFromSet(invalidXMLCharacterSet)
+    }
+
+    return cleanedString
+}
+
+
+
+private func findTestSummariesPlistFile(logsTestPath logsTestPath: String) -> String {
+    let summariesPlistSuffix = "TestSummaries.plist"
+    var summariesPlistFile = ""
+    var logsTestFiles = [String]()
+    let fileManager = NSFileManager.defaultManager()
+    if fileManager.fileExistsAtPath(logsTestPath) {
+        do {
+            logsTestFiles = try fileManager.contentsOfDirectoryAtPath(logsTestPath)
+        } catch let e {
+            try! CustomErrorType.InvalidState(error: "Error when getting files from \(logsTestPath) path : \(e)").throwsError()
+        }
+    }
+    for file in logsTestFiles {
+        if file.hasSuffix(summariesPlistSuffix) {
+            summariesPlistFile = logsTestPath + "/" + file
+            break
+        }
+    }
+    return summariesPlistFile
+}
+
+private func getTestSummariesPlistJson(logsTestPath logsTestPath: String) -> JSON {
+    let summariesPlistFile = findTestSummariesPlistFile(logsTestPath: logsTestPath)
+
+    let summariesPlistDict = (NSDictionary(contentsOfFile: summariesPlistFile) as? Dictionary<String, AnyObject>) ?? Dictionary<String, AnyObject>()
+    return JSON(summariesPlistDict)
+}
+
 /// Save the last @screenshotsCount screenshots to @lastScreenshotsPath folder from @logsTestPath test logs for failed tests
 /// if screenshotsCount is -1 then save all screenshots available
 /// - parameter excludeIdenticalScreenshots: excludes the consecutive identical screenshots, to get the relevant screenshots
-func saveLastScreenshots(logsTestPath logsTestPath: String, lastScreenshotsPath: String, screenshotsCount: Int, excludeIdenticalScreenshots: Bool = false) {
+func saveLastScreenshots(testSummariesPlistJson testSummariesPlistJson: JSON, logsTestPath: String, lastScreenshotsPath: String, screenshotsCount: Int, excludeIdenticalScreenshots: Bool = false) {
     print("Save last \(screenshotsCount) screenshots from \(logsTestPath) logs test folder to \(lastScreenshotsPath) folder")
     if logsTestPath.isEmpty {
         try! CustomErrorType.InvalidArgument(error: "Tests logs path is empty.").throwsError()
@@ -72,15 +153,11 @@ func saveLastScreenshots(logsTestPath logsTestPath: String, lastScreenshotsPath:
 
     let fileManager = NSFileManager.defaultManager()
     let appScreenShotsPath = logsTestPath + "/Attachments/"
-    let summariesPlistFile = findTestSummariesPlistFile(logsTestPath: logsTestPath)
-
-    let summariesPlistDict = (NSDictionary(contentsOfFile: summariesPlistFile) as? Dictionary<String, AnyObject>) ?? Dictionary<String, AnyObject>()
-    let summariesPlistJson = JSON(summariesPlistDict)
     let testJsonPath: [SubscriptType] = ["^", "TestableSummaries", ".", "Tests", ".", "Subtests", ".", "Subtests", ".", "Subtests", "."]
     let testStatusJsonPath: [SubscriptType] = testJsonPath + ["TestStatus"]
     let testIdentifierJsonPath: [SubscriptType] = ["TestIdentifier"]
     // extract the failed test nodes for finding the test screenshots
-    let failedTests = summariesPlistJson.getParentValuesFor(relativePath: testStatusJsonPath, withValue: JSON("Failure"))
+    let failedTests = testSummariesPlistJson.getParentValuesFor(relativePath: testStatusJsonPath, withValue: JSON("Failure"))
     for failedTestNode in failedTests {
         let testIdentifier = failedTestNode[testIdentifierJsonPath].stringValue
         let testLastScreenShotsPath = lastScreenshotsPath + "/\(testIdentifier.stringByReplacingOccurrencesOfString("/", withString: "_"))/"
@@ -128,35 +205,9 @@ func saveLastScreenshots(logsTestPath logsTestPath: String, lastScreenshotsPath:
     }
 }
 
-private func findTestSummariesPlistFile(logsTestPath logsTestPath: String) -> String {
-    let summariesPlistSuffix = "TestSummaries.plist"
-    var summariesPlistFile = ""
-    var logsTestFiles = [String]()
-    let fileManager = NSFileManager.defaultManager()
-    if fileManager.fileExistsAtPath(logsTestPath) {
-        do {
-            logsTestFiles = try fileManager.contentsOfDirectoryAtPath(logsTestPath)
-        } catch let e {
-            try! CustomErrorType.InvalidState(error: "Error when getting files from \(logsTestPath) path : \(e)").throwsError()
-        }
-    }
-    for file in logsTestFiles {
-        if file.hasSuffix(summariesPlistSuffix) {
-            summariesPlistFile = logsTestPath + "/" + file
-            break
-        }
-    }
-    return summariesPlistFile
-}
-
 /// Generate JUnit report xml file from TestSummaries plist file from @logsTestPath logs test folder at path @jUnitRepPath
-func generateJUnitReport(logsTestPath logsTestPath: String, jUnitRepPath: String) {
+func generateJUnitReport(testSummariesPlistJson testSummariesPlistJson: JSON, logsTestPath: String, jUnitRepPath: String) {
     print("Generate JUnit report xml file from \(logsTestPath) logs test folder to \(jUnitRepPath) file")
-
-    let summariesPlistFile = findTestSummariesPlistFile(logsTestPath: logsTestPath)
-
-    let summariesPlistDict = (NSDictionary(contentsOfFile: summariesPlistFile) as? Dictionary<String, AnyObject>) ?? Dictionary<String, AnyObject>()
-    let summariesPlistJson = JSON(summariesPlistDict)
 
     // parse the TestSummaries plist file and create the JUnit xml document
     let testSuitesNode = NSXMLElement(name: "testsuites")
@@ -177,7 +228,7 @@ func generateJUnitReport(logsTestPath logsTestPath: String, jUnitRepPath: String
     let fileNameJsonPath: [SubscriptType] = ["FileName"]
     let lineNumberJsonPath: [SubscriptType] = ["LineNumber"]
 
-    let testableSummariesJsons = summariesPlistJson[testableSummariesJsonPath].arrayValue
+    let testableSummariesJsons = testSummariesPlistJson[testableSummariesJsonPath].arrayValue
     var totalTestsCount = 0
     var totalFailuresCount = 0
     for testableSummaryJson in testableSummariesJsons {
@@ -206,7 +257,7 @@ func generateJUnitReport(logsTestPath logsTestPath: String, jUnitRepPath: String
                     let failureSummariesJson = testCaseJson[failureSummariesJsonPath]
                     if failureSummariesJson.arrayValue.count > 0 {
                         let firstFailureSummaryJson = failureSummariesJson[0]
-                        failureMessage = firstFailureSummaryJson[messageJsonPath].stringValue
+                        failureMessage = validXMLString(firstFailureSummaryJson[messageJsonPath].stringValue)
                         var fileName = firstFailureSummaryJson[fileNameJsonPath].stringValue
                         let rangeToRemove = fileName.rangeOfString(targetName + "/")
                         fileName.replaceRange(fileName.startIndex..<(rangeToRemove?.startIndex ?? fileName.startIndex), with: "")
@@ -218,7 +269,7 @@ func generateJUnitReport(logsTestPath logsTestPath: String, jUnitRepPath: String
                     let failureNode = NSXMLElement(name: "failure", stringValue: failureStackTrace)
                     let messageAttr = NSXMLNode.attributeWithName("message", stringValue: failureMessage)  as! NSXMLNode
                     failureNode.attributes = [messageAttr]
-                    let systemOutNode = NSXMLElement(name: "system-out", stringValue: outputLogs.joinWithSeparator("\n"))
+                    let systemOutNode = NSXMLElement(name: "system-out", stringValue: validXMLString(outputLogs.joinWithSeparator("\n")))
                     testCaseNode.addChild(failureNode)
                     testCaseNode.addChild(systemOutNode)
                 }
@@ -275,8 +326,6 @@ func generateJUnitReport(logsTestPath logsTestPath: String, jUnitRepPath: String
 
 // ====== main ======
 
-//let args = ["", "--logsTestPath", "Logs/Test", "--jUnitReportPath", "junit-report.xml"]
-
 // ====== available options ======
 let logsTestPathOption = "logsTestPath"
 let jUnitReportPathOption = "jUnitReportPath"
@@ -316,16 +365,19 @@ if let screenshotsCountOptionValue = screenshotsCountOptionValue {
 // exclude the consecutive identical screenshots if --excludeIdenticalScreenshots option is present
 var excludeIdenticalScreenshots = excludeIdenticalScreenshotsOptionValue != nil
 
+let logsTestPath = logsTestPathOptionValue!
+let testSummariesPlistJson = getTestSummariesPlistJson(logsTestPath: logsTestPath)
+
 // generate the report if --jUnitReportPath option is passed
 if let jUnitReportPathOptionValue = jUnitReportPathOptionValue {
     argumentOptionsParser.validateOptionIsNotEmpty(optionName: jUnitReportPathOption, optionValue: jUnitReportPathOptionValue)
 
-    generateJUnitReport(logsTestPath: logsTestPathOptionValue!, jUnitRepPath: jUnitReportPathOptionValue)
+    generateJUnitReport(testSummariesPlistJson: testSummariesPlistJson, logsTestPath: logsTestPath, jUnitRepPath: jUnitReportPathOptionValue)
 }
 
 // save the last screenshots if --screenshotsPath option is passed
 if let screenshotsPathOptionValue = screenshotsPathOptionValue {
     argumentOptionsParser.validateOptionIsNotEmpty(optionName: screenshotsPathOption, optionValue: screenshotsPathOptionValue)
     
-    saveLastScreenshots(logsTestPath: logsTestPathOptionValue!, lastScreenshotsPath: screenshotsPathOptionValue, screenshotsCount: screenshotsCount, excludeIdenticalScreenshots: excludeIdenticalScreenshots)
+    saveLastScreenshots(testSummariesPlistJson: testSummariesPlistJson, logsTestPath: logsTestPath, lastScreenshotsPath: screenshotsPathOptionValue, screenshotsCount: screenshotsCount, excludeIdenticalScreenshots: excludeIdenticalScreenshots)
 }
